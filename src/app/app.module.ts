b58b04af23c97aacbfe4a6070315a632988f6799
Charith <rajithacharith@gmail.com>
2018-11-20 15:56:07 +0530

    add paper list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PaperComponent } from './paper/paper.component';
+import { PaperListComponent } from './paper-list/paper-list.component';
+import { StudentNavComponent } from './student-nav/student-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PaperComponent
+    PaperComponent,
+    PaperListComponent,
+    StudentNavComponent
   ],
   imports: [
     BrowserModule

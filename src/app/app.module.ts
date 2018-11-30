bef0ceabe619b1a985652cd237c54aff867f77fb
Charith <rajithacharith@gmail.com>
2018-11-30 21:22:05 +0530

    added answers to local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
--- src/app/paper/paper.component.ts
+++ src/app/paper/paper.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataserviceService } from '../dataservice.service';
 import { SharedserviceService } from '../services/sharedservice.service';
+
 import { CountdownModule } from 'ngx-countdown';
 import { NgModel } from '@angular/forms';
 
@@ -13,7 +14,7 @@ import { NgModel } from '@angular/forms';
 
 
 export class PaperComponent implements OnInit {
-  private storageName = 'paperDetails';
+  //private storageName = 'paperDetails';
   public paperSet :any;
   public stream : string ;
   public year : number ;
@@ -21,37 +22,37 @@ export class PaperComponent implements OnInit {
 
   public answers : any;
   //public questionID : any;
-  public studentID : number;
+  public studentID: number;
   public paperID: number;
   public questionSet: any;
-  public answerSet : any;
-  selectedValue :string ;
+  public answerSet: any;
+  selectedValue: string ;
+  public studentAnswers: {};
+  public answerArr: {};
 
-
-  
   correctAnswerSet = [];
   markedans = [];
 
-  showSpinner : boolean = true ;
+  showSpinner = true ;
 
-  
 
-  studentAnswers = {};
-  message : any;
+
+
+  message: any;
   constructor( private dataService : DataserviceService, private shared : SharedserviceService ) {
 
 
 
-    
+
     this.studentID = 160292;
-    //this.questionID = 101;
+    // this.questionID = 101;
 
     this.dataService.getQuestionFilterByPaperID(this.paperID).subscribe((item)=>{
       this.questionSet = item;
-      console.log("the questions are");
+      console.log("the questions are ::");
       console.log(this.questionSet);
     });
-    
+
 
 
   }
@@ -64,90 +65,121 @@ export class PaperComponent implements OnInit {
     console.log(this.correctAnswerSet)
 
   }
-  
-  radioChangeHandle(event : any,questionIndex:number,answerIndex : number){
+
+  radioChangeHandle(event: any, questionIndex: number, answerIndex: number) {
 
     const answerObject = {
       questionNumber : questionIndex,
       answerNumber : answerIndex,
       answerValue : event.target.value
-    }
+    };
 
     this.studentAnswers[answerObject.questionNumber] = answerObject.answerNumber;
-    console.log('ans:',this.studentAnswers)
-    
-    
+    console.log('ans:', this.studentAnswers);
+    localStorage.setItem('answers', JSON.stringify(this.studentAnswers));
+
+
 
   }
-  checkpaper(){
-      
+  checkpaper() {
+
       this.createCorrectAnsArr();
-      
-      console.log("student answers")
-      console.log(this.studentAnswers)
-      for(var i = 0;i<this.correctAnswerSet.length;i++) { 
-        if(this.studentAnswers[i]==this.correctAnswerSet[i]){
+
+      console.log('student answers');
+      console.log(this.studentAnswers);
+      for (let i = 0; i < this.correctAnswerSet.length; i++) {
+        if (this.studentAnswers[i] == this.correctAnswerSet[i]){
             this.markedans.push(true);
-        }
-        else{
+        } else {
           this.markedans.push(false);
         }
       }
-      console.log("marked answers")
+      console.log('marked answers');
       console.log(this.markedans);
-      console.log(this.correctAnswerSet)
-      var mark = this.markedans
+      console.log(this.correctAnswerSet);
+      let mark = this.markedans;
 
       this.dataService.storeMarkedAnswers(this.studentID,this.paperID,this.markedans).subscribe(()=>{
-        console.log("Item recorded!")
+        console.log('Item recorded!')
       });
 
-      
+
       return this.markedans;
     }
 
-  setPaperDetails(){
-    let details = {
+  setPaperDetails() {
+    const details = {
       'stream' : this.stream,
       'year': this.year ,
       'subject': this.subject,
       'paperID': this.paperID
-    }
-    localStorage.setItem(this.storageName, JSON.stringify(details));
+    };
+    localStorage.setItem('PaperDetails', JSON.stringify(details));
   }
 
-  finishPaper(){
+  finishPaper() {
     console.log('time out');
   }
 
   ngOnInit() {
-    this.shared.currentMessage.subscribe(message => {
-      this.message = message;
-    });
-    console.log(this.message[0].stream);
-    this.stream = this.message[0].stream;
-    this.year = this.message[0].year;
-    this.subject = this.message[0].subject;
-    this.paperID = this.message[0].paperID;
-    // this above part should be avoided in order to maintain the local storage data even after the refresh
-    // when we refresh the page these above lines are executed and all local variables become null.
-    console.log(this.stream , this.year, this.subject, this.paperID);
 
-    this.dataService.filterPaperByAll(this.stream,this.subject,this.year).subscribe((paper) => {
-      this.paperSet = paper;
-      this.showSpinner = false;
-      console.log(paper);
-    });
+    if (!(localStorage.getItem('PaperDetails') === null)) {
+      console.log('Entered if');
+      console.log(localStorage.getItem('PaperDetails'));
+      const paperDetails = JSON.parse(localStorage.getItem('PaperDetails')) ;
+      console.log(paperDetails);
+      this.stream = paperDetails.stream;
+      this.year = paperDetails.year;
+      this.subject = paperDetails.subject;
+      this.paperID = paperDetails.paperID;
 
-    this.dataService.getAnswers().subscribe((answers) => {
-      this.answers=answers;
-    });
+      this.dataService.filterPaperByAll(this.stream,this.subject,this.year).subscribe((paper) => {
+        this.paperSet = paper;
+        this.showSpinner = false;
+        console.log(paper);
+      });
 
-    this.dataService.getQuestionFilterByPaperID(this.paperID).subscribe((question)=>{
-      this.questionSet = question;
-    });
-    this.setPaperDetails();
+      if (!(localStorage.getItem('answers') === null)){
+        this.studentAnswers = JSON.parse(localStorage.getItem('answers'));
+        console.log(this.studentAnswers);
+      }
+      this.dataService.getAnswers().subscribe((answers) => {
+        this.answers=answers;
+      });
 
-  }
+      this.dataService.getQuestionFilterByPaperID(this.paperID).subscribe((question)=>{
+        this.questionSet = question;
+      });
+
+
+    } else {
+        this.shared.currentMessage.subscribe(message => {
+          this.message = message;
+        });
+        console.log(this.message[0].stream);
+        this.stream = this.message[0].stream;
+        this.year = this.message[0].year;
+        this.subject = this.message[0].subject;
+        this.paperID = this.message[0].paperID;
+        console.log(this.stream , this.year, this.subject, this.paperID);
+
+        this.dataService.filterPaperByAll(this.stream,this.subject,this.year).subscribe((paper) => {
+          this.paperSet = paper;
+          this.showSpinner = false;
+          console.log(paper);
+        });
+
+        this.dataService.getAnswers().subscribe((answers) => {
+          this.answers = answers;
+        });
+
+        this.dataService.getQuestionFilterByPaperID(this.paperID).subscribe((question)=>{
+          this.questionSet = question;
+        });
+        this.setPaperDetails();
+
+      }
+    }
+
 
 }
